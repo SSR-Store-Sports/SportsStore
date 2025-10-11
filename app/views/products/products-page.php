900c3f3b760e67694f4c0a46e876e5833639b4f5
caique-dev-01 <jcaique825@gmail.com>
2025-10-10 23:56:16 -0300

    ✨Feat: layout de cards e botões do catálogo

--- app/views/products/products-page.php
+++ app/views/products/products-page.php
@@ -1,6 +1,63 @@
 <!DOCTYPE html>
 
 <body class="bg-black text-white">
+<section class="section-botoes">
+  <button class="btn active">PRODUTOS</button>
+  <button class="btn">TOP’S</button>
+  <button class="btn">CALÇAS</button>
+  <button class="btn">SHORTS</button>
+  <button class="btn">CONJUNTOS</button>
+</section>
+
+<section>
+<div class="card-1">
+    <img src="c:\Users\jcaiq\Downloads\image 14.jpg" class="card-img-top" alt="Conjunto Top Fitness">
+    <div class="card-body">
+        <h3 class="card-title">TOP FITNESS</h3>
+        <p class="card-price">R$ 79,90</p>
+        <p class="card-installments">10x de R$ 7,99</p>
+        <p class="card-description">Conjunto fitness com bojo, calça legging e top cropped.</p>
+        <a href="#" class="btn btn-primary">COMPRAR</a>
+    </div>
+</div>
+
+<div>
+<div class="card-2">
+    <img src="c:\Users\jcaiq\Downloads\image 15.jpg" class="card-img-top" alt="Conjunto Top Fitness">
+    <div class="card-body">
+        <h3 class="card-title">CONJUNTO FITNESS</h3>
+        <p class="card-price">R$ 89,90</p>
+        <p class="card-installments">10x de R$ 8,99</p>
+        <p class="card-description">Conjunto fitness com bojo, calça legging e top cropped.</p>
+        <a href="#" class="btn btn-primary">COMPRAR</a>
+    </div>
+</div>
+</div>
+
+<div class="card-3">
+    <img src="images/image14.jpg" alt="Conjunto Top Fitness">
+    <div class="card-body">
+        <h3 class="card-title">CONJUNTO FITNESS</h3>
+        <p class="card-price">R$ 99,90</p>
+        <p class="card-installments">10x de R$ 9,99</p>
+        <p class="card-description">Conjunto fitness com bojo, calça legging e top cropped.</p>
+        <a href="#" class="btn btn-primary">COMPRAR</a>
+    </div>
+</div> 
+</section>
+
 </body>
 
 </html>
+
+<style> 
+.btn {
+border: 4px solid #000;
+}
+.card-1 {
+border: 4px solid #000;
+}
+.card-body {
+border: 4px solid #000;
+}
+</style>
