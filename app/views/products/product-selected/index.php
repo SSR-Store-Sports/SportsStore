8745e07c72d3b88cfcbd0ba8dc2d2d4f0a4fe6f0
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-10-21 16:52:15 -0300

    ✨ Feat: Add product selected page

---
+++ app/views/products/product-selected/index.php
@@ -0,0 +1,30 @@
+  <!-- CSS externo -->
+<link rel="stylesheet" href="app\views\products\product\styles.css">
+
+<body class="bg-black text-white">
+  <div class="product">
+    <h2>Top Fitness</h2>
+    <img src="/public/images/product.jpg" alt="" />
+
+    <div class="deitals-product">
+      <div class="price-product">
+        <h3>R$ 50,00</h3>
+        <p>ou 2X de 25,00</p>
+      </div>
+    </div>
+
+    <div>
+      <h3>Descrição</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+      </p>
+    </div>
+
+    <div>
+      <h3>Cor</h3>
+    </div>
+
+    <button>Adiconar ao carrinho</button>
+  </div>
+</body>
+</html>
