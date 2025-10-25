b7776f3824a02807d7749d0268150acfa4df98e1
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-10-25 20:09:40 -0300

    ✨ Feat: Add font Inter at project and organize codebase index

--- index.php
+++ index.php
@@ -2,13 +2,18 @@
 <html lang="pt-br">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="TatiFit Sports - Loja online de roupas esportivas femininas">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="TatiFit Sports - Loja online de roupas esportivas femininas" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
 
   <title>TatiFit Wear</title>
-  <link rel="icon" href="/public/images/favicon.png">
-  <link rel="stylesheet" href="/globals.css">
+  <link rel="icon" href="/public/images/favicon.png" />
+  <link rel="stylesheet" href="/globals.css" />
+  
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
 
   <link rel="stylesheet" type="text/css"
     href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css" />
