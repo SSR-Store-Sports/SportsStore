798b65d2b027f212acb5ef21bd9eb85927be102d
caique-dev-01 <caique.alves02@gmail.com>
2025-11-01 02:50:16 -0300

    🌟 Feat: Add styles for the address-content section

--- app/views/users/address/index.php
+++ app/views/users/address/index.php
@@ -5,28 +5,24 @@
     <div class="address-header">
       <h1 class="">Endereço de Entrega</h1>
     </div>
-    <div class="address-content">
+    <section class="address-content">
       <div class="address-items">
-        <h1>Resumo do pedido</h1>
-        <div class="item-image"><img src="public\images\conjunto-fit.jpg" alt="Legging Fitness"></div>
+        <h2>Resumo do pedido</h2>
+        <div class="item-image"><img src="public/images/conjunto-fit.jpg" alt="Legging Fitness"></div>
         <div class="item-details">
           <h3>Top Linha Premium</h3>
           <p class="color-item">Preto</p>
           <p class="item-size">G</p>
+          <div class="price-old">R$ 119,90</div>
+          <div class="price-current">R$ 89,90</div>
+          </div>
+          <div class="add-address">
+          <button id="add-address-button" class="btn-add-address"><i class="ph ph-plus"></i></button>
+          <p>Cadastre seu melhor endereço</p>
 
         </div>
 
       </div>
-
-      
-
-    </div>
-
-
-
-
-
-
-
+    </section>
   </main>
 </body>
