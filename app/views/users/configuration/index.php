29382a80a99c798388723945d7ecea4c5f0dc273
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-11-02 21:12:11 -0300

    ✨ Feat: Add page of configuration profile

---
+++ app/views/users/configuration/index.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="/app/views/users/configuration/styles.css">
+
+<body>
+<main class="checking">
+
+<div class="image_check">
+    <img class="check" src="/public\images/check.png" alt="">
+        </div>
+
+    <h1>
+        Conta criada com sucesso!
+    </h1>
+</div>
+
+<div class="text-p">
+    <p>
+    Bem-vinda(o) à TatiFit!
+    Agora você pode fazer login e começar suas compras.
+    </p>
+</div>
+</main>
+</body>
