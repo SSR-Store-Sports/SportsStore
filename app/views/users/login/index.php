320267eca4d6293e8fc28261ce969038eb9d116c
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-11-02 21:12:55 -0300

    ✨ Feat: Add adjust in login page styles

--- app/views/users/login/index.php
+++ app/views/users/login/index.php
@@ -1,40 +1,40 @@
-<link rel="stylesheet" href="/app/views/users/register/styles.css">
+<link rel="stylesheet" href="/app/views/users/login/styles.css">
 
-<body class="bg-black text-white">
-<main class="login-main">
-    <div class="carrosel">
-        <img class="modelo1" src="/public\images/modelo1.jpg" alt="Modelo 1">
-        <img class="modelo2" src="/public\images/modelo2.jpg" alt="Modelo 2">
-        
-    </div>
-
-    <div class="login-container">
-        <form>
-            <div class="login-form">
-                <div class="logo">
-                    <img src="/public/images/logo.png" alt="TatiFit Wear">
-                </div>
-                <div class="form-group">
-                    <input type="email" id="email" name="email" placeholder="Email">
-                </div>
-                <div class="form-group">
-                    <input type="password" id="password" name="password" placeholder="Senha">
-                    <a href="#">Esqueci a senha</a>
-                </div>
-                <button type="submit" class="btn-login">Entrar</button>
-                <p>Não tem conta? <a href="#">Criar conta</a></p>
-        </form>
-
-        <div class="social-login">
-            <button class="google-login">
-                <img src="/public/images/google-icon1.png" alt="Google" />
-                Entrar com Google
-            </button>
-            <button class="facebook-login">
-                <img src="/public/images/facebook-icon1.png" alt="Facebook">
-                Entrar com Facebook
-            </button>
+<body>
+    <main class="login-main">
+        <div class="carrosel">
+            <img class="modelo1" src="/public\images/modelo1.jpg" alt="Modelo 1">
+            <img class="modelo2" src="/public\images/modelo2.jpg" alt="Modelo 2">
         </div>
-    </div>
-</main>
+
+        <div class="login-container">
+            <form>
+                <div class="login-form">
+                    <div>
+                        <img class="logo-login" src="/public/images/logo.png" alt="TatiFit Wear">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" id="email" name="email" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" id="password" name="password" placeholder="Senha">
+                        <a href="#">Esqueci a senha</a>
+                    </div>
+                    <button type="submit" class="btn-login">Entrar</button>
+                    <p>Não tem conta? <a href="#">Criar conta</a></p>
+                </div>
+            </form>
+
+            <!-- <div class="social-login">
+                <button class="google-login">
+                    <img src="/public/images/google-icon1.png" alt="Google" />
+                    Entrar com Google
+                </button>
+                <button class="facebook-login">
+                    <img src="/public/images/facebook-icon1.png" alt="Facebook">
+                    Entrar com Facebook
+                </button>
+            </div> -->
+        </div>
+    </main>
 </body>
