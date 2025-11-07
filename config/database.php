ab8a596cd034364d8346502ebcbbfd33048ce182
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-11-07 16:49:46 -0300

    ✨ Feat: Add config database using PDO

--- config/database.php
+++ config/database.php
@@ -1,2 +1,15 @@
 <?php
-?>
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "tatifit_database";
+$port = 3306;
+
+// stmt prepare gets
+
+try {
+    $db = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+}
