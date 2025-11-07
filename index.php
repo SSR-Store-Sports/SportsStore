3b3a3a672ef523d6800c5bda5925b863246218a6
RODRIGO AVELINO ARRUDA DA SILVA <rodrigo.aasilva3@senacsp.edu.br>
2025-11-07 20:47:27 -0300

    ✨ Feat: Integrate VLibras widget for global accessibility

--- index.php
+++ index.php
@@ -26,6 +26,17 @@
     // require_once 'config/database.php';
     require_once 'routes/web.php';
   ?>
+
+  <div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  </script>
 </body>
 
 </html>
