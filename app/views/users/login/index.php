5bae8f224c07174ea1dc379cd55b0d80fd9c2efe
RODRIGO AVELINO ARRUDA DA SILVA <rodrigo.aasilva3@senacsp.edu.br>
2025-11-07 22:02:49 -0300

    🐛 Fix: Adjusted login page for both desktop and mobile views

--- app/views/users/login/index.php
+++ app/views/users/login/index.php
@@ -1,40 +1,31 @@
-<link rel="stylesheet" href="/app/views/users/register/styles.css">
+<link rel="stylesheet" href="/app/views/users/login/styles.css">
 
 <body class="bg-black text-white">
 <main class="login-main">
-    <div class="carrosel">
-        <img class="modelo1" src="/public\images/modelo1.jpg" alt="Modelo 1">
-        <img class="modelo2" src="/public\images/modelo2.jpg" alt="Modelo 2">
+    <div class="carrosel-login">
+        <img class="modelo1-login" src="/public\images/modelo1.jpg" alt="Modelo 1">
+        <img class="modelo2-login" src="/public\images/modelo2.jpg" alt="Modelo 2">
         
     </div>
 
     <div class="login-container">
         <form>
             <div class="login-form">
-                <div class="logo">
-                    <img src="/public/images/logo.png" alt="TatiFit Wear">
+                <div class="logo-login">
+                    <img class="logo-login-img" src="/public/images/logo.png" alt="TatiFit Wear">
                 </div>
-                <div class="form-group">
+                <div class="form-email-password">
                     <input type="email" id="email" name="email" placeholder="Email">
                 </div>
-                <div class="form-group">
+                <div class="form-email-password">
                     <input type="password" id="password" name="password" placeholder="Senha">
                     <a href="#">Esqueci a senha</a>
                 </div>
                 <button type="submit" class="btn-login">Entrar</button>
-                <p>Não tem conta? <a href="#">Criar conta</a></p>
+                <p class="p-login">Não tem conta? <a href="#">Criar conta</a></p>
+            </div>    
         </form>
 
-        <div class="social-login">
-            <button class="google-login">
-                <img src="/public/images/google-icon1.png" alt="Google" />
-                Entrar com Google
-            </button>
-            <button class="facebook-login">
-                <img src="/public/images/facebook-icon1.png" alt="Facebook">
-                Entrar com Facebook
-            </button>
-        </div>
     </div>
 </main>
 </body>
