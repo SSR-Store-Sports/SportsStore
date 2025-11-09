69280213f5e5ccd0409086f8b2f7dc14e460585b
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-11-09 19:00:15 -0300

    🐍  Fix/Feat/Refactor: Add adjust in styles of pages - products, header, sidebar, configuration / Add admin pages for register products and supplier, etc... / Add adjust in folders

---
+++ app/views/_components/header/index.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="/app/views/_components/header/styles.css">
+<script src="/public/js/search.js"></script>
+
+<header>
+    <nav class="navbar">
+        <button class="menu-toggle" aria-label="Menu">
+            <i class="ph ph-list"></i>
+        </button>
+
+        <div class="logo">
+            <a href="/">
+                <img width="50px" src="/public/images/logo.png" alt="TatiFit Sports Logo" />
+            </a>
+        </div>
+
+        <div class="nav-actions">
+            <button class="cart-btn" aria-label="Carrinho">
+                <a href="/cart"><i class="ph ph-shopping-cart"></i></a>
+            </button>
+            <div class="search-container">
+                <input type="text" id="searchInput" placeholder="Buscar produtos..." class="search-input" style="display: none;">
+                <button class="search-btn" onclick="toggleSearch()" aria-label="Buscar">
+                    <i class="ph ph-magnifying-glass"></i>
+                </button>
+            </div>
+        </div>
+    </nav>
+</header>
