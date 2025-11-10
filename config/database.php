108e0b9e17b75a5e0dd1808eccbfb15322d7538e
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-11-10 16:51:06 -0300

    ✨ Feat: Add adjust using options for PDO and add variable of environment

--- config/database.php
+++ config/database.php
@@ -1,13 +1,26 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "tatifit_database";
-$port = 3306;
+
+require_once __DIR__ . '/../app/env/index.php';
+use DevCoder\DotEnv;
+(new DotEnv(__DIR__ . '/../.env'))->load();
+
+$servername = trim(getenv('HOST'), '" ');
+$port = trim(getenv('PORT'), '" ');
+$username = trim(getenv('USERNAME'), '" ');
+$password = trim(getenv('PASSWORD'), '" ');
+$database = trim(getenv('DATABASE'), '" ');
+$ssl_ca_path = 'certs\ca.pem';
 
 try {
-    $db = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $options = [
+        PDO::MYSQL_ATTR_SSL_CA => $ssl_ca_path, 
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_TIMEOUT => 10
+    ];
+
+    $db = new PDO("mysql:host=$servername;dbname=$database;charset=utf8;port=$port", $username, $password);
+
+    echo "Conexão SSL em PHP estabelecida com sucesso!";
 } catch (PDOException $e) {
-    echo "Erro: " . $e->getMessage();
+    error_log("Erro de Conexão: " . $e->getMessage());
 }
