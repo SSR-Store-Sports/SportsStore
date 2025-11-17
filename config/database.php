3f3e7ac09bfdc6d2f2059b525454111ed5b38bbf
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-11-17 16:52:50 -0300

    ✨ Feat: Add parameters security in PDO database

--- config/database.php
+++ config/database.php
@@ -11,15 +11,16 @@ $password = trim(getenv('PASSWORD'), '" ');
 $database = trim(getenv('DATABASE'), '" ');
 $ssl_ca_path = 'certs\ca.pem';
 
+$options = [
+    // PDO::MYSQL_ATTR_SSL_CA => $ssl_ca_path, 
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // throw exception
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // data return array assoc
+    PDO::ATTR_EMULATE_PREPARES => false, // Protected Injection SQL / Controller Preprend Commands
+    PDO::ATTR_TIMEOUT => 10,
+];
+
 try {
-    $options = [
-        PDO::MYSQL_ATTR_SSL_CA => $ssl_ca_path, 
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_TIMEOUT => 10
-    ];
-
     $db = new PDO("mysql:host=$servername;dbname=$database;charset=utf8;port=$port", $username, $password);
-
     // echo "Conexão SSL em PHP estabelecida com sucesso!";
 } catch (PDOException $e) {
     error_log("Erro de Conexão: " . $e->getMessage());
